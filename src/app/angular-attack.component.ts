9f1ce85e0e995c5c23cb6415b758b3afab02ed24
Healbadbad <healbadbad@yahoo.com>
2016-05-13 20:42:12 -0400

    created basic navbar

--- src/app/angular-attack.component.ts
+++ src/app/angular-attack.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterializeDirective } from 'angular2-materialize';
+import { NavbarComponent } from './navbar';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { MaterializeDirective } from 'angular2-materialize';
   templateUrl: 'angular-attack.component.html',
   styleUrls: ['angular-attack.component.css'],
 
-  directives: [MaterializeDirective]
+  directives: [MaterializeDirective, NavbarComponent]
 
 })
 export class AngularAttackAppComponent {
