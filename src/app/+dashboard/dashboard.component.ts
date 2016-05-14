6cc38923433b6c00e1a626d9cb942280dd8c7744
rosspa <rosspa@rose-hulman.edu>
2016-05-14 08:10:44 -0400

    partially implemented the login service. (you have get the currentUser and logOut)

--- src/app/+dashboard/dashboard.component.ts
+++ src/app/+dashboard/dashboard.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {MyServiceService} from "../services/my-service.service";
-import { WeekPlanCardComponent } from '../week-plan-card';
+import {Component, OnInit} from "@angular/core";
+import {MyServiceService} from "../services/my-service/my-service.service";
+import {WeekPlanCardComponent} from "../week-plan-card";
+
 
 @Component({
   moduleId: module.id,
-  selector: 'app-dashboard',
-  templateUrl: 'dashboard.component.html',
-  styleUrls: ['dashboard.component.css'],
+  selector: "app-dashboard",
+  templateUrl: "dashboard.component.html",
+  styleUrls: ["dashboard.component.css"],
 
   directives: [WeekPlanCardComponent],
   providers: [MyServiceService]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private myService:MyServiceService) {}
+  constructor(private myService: MyServiceService){
+  }
 
   ngOnInit() {
-    console.log(this.myService.myFunc());
   }
-
-  testMyService() {
-    console.log(this.myService.myFunc());
-  }
-
-
 }
