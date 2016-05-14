bcaafc11cf56ab7da2b72361168fb3cf492ddff4
chinchalupa <wrightjt@rose-hulman.edu>
2016-05-14 14:56:01 -0400

    Created and added footer and home route
    Generated home route.
    Generated footer component.

--- src/app/angular-attack.component.ts
+++ src/app/angular-attack.component.ts
@@ -1,30 +1,35 @@
-import { Component } from '@angular/core';
-import { MaterializeDirective } from 'angular2-materialize';
-import { NavbarComponent } from './navbar';
-import { Routes, ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
-import { DashboardComponent } from './+dashboard'
+import {Component} from '@angular/core';
+import {MaterializeDirective} from 'angular2-materialize';
+import {NavbarComponent} from './navbar';
+import {Routes, ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from '@angular/router';
+import {DashboardComponent} from './+dashboard'
 import {SessionListComponent} from "./+session-list/session-list.component";
 import {StatisticsComponent} from "./+statistics/statistics.component";
+import {HomeComponent} from "./+home/home.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
-  moduleId: module.id,
-  selector: 'angular-attack-app',
-  templateUrl: 'angular-attack.component.html',
-  styleUrls: ['angular-attack.component.css'],
+    moduleId: module.id,
+    selector: 'angular-attack-app',
+    templateUrl: 'angular-attack.component.html',
+    styleUrls: ['angular-attack.component.css'],
 
-  directives: [MaterializeDirective,
-    NavbarComponent,
-    DashboardComponent,
-    ROUTER_DIRECTIVES],
-    
-  providers: [ROUTER_PROVIDERS]
+    directives: [MaterializeDirective,
+        NavbarComponent,
+        DashboardComponent,
+        FooterComponent,
+        ROUTER_DIRECTIVES],
+
+    providers: [ROUTER_PROVIDERS]
 })
 
 @Routes([
-	{path:"/dashboard",component:DashboardComponent},
+    {path: '/', component: HomeComponent},
+    {path: "/dashboard", component: DashboardComponent},
     {path: '/session', component: SessionListComponent},
     {path: '/statistics', component: StatisticsComponent}
+
 ])
 export class AngularAttackAppComponent {
-  title = 'Team Hairforce';
+
 }
