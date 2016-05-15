e16cc7cf349d8dfd8717e2ffeec61f67cffd3887
chinchalupa <wrightjt@rose-hulman.edu>
2016-05-14 22:49:58 -0400

    Tiles for exercise links now work properly
    Tiles now work properly.
    Loading figured out for elements.

--- src/app/+exercise/exercise.component.ts
+++ src/app/+exercise/exercise.component.ts
@@ -3,6 +3,8 @@ import {SimilarExercisesComponent} from "../similar-exercises/similar-exercises.
 import {Exercise} from "../models/exercise";
 import {ExerciseCardComponent} from "../exercise-card/exercise-card.component";
 // import { RouterParams } from "@angular/router";
+import {RouteSegment} from '@angular/router';
+import {ExerciseService} from "../services/exercise/exercise.service";
 
 @Component({
     moduleId: module.id,
@@ -13,17 +15,34 @@ import {ExerciseCardComponent} from "../exercise-card/exercise-card.component";
     directives: [
         SimilarExercisesComponent,
         ExerciseCardComponent
+    ],
+
+    providers: [
+        ExerciseService
     ]
 })
 export class ExerciseComponent implements OnInit {
 
-    @Input() exercise:Exercise;
+    exercise:Exercise;
+    id:string;
+    isLoading:boolean;
 
-    constructor() {
+    constructor(private exerciseService:ExerciseService) {
+        this.isLoading = true;
     }
 
     ngOnInit() {
         // let id = +this.routeParams.get('id');
+        this.exerciseService.getExercise(this.id, (exercise) => {
+            this.exercise = exercise;
+            this.isLoading = false;
+        });
+    }
+
+    routerOnActivate(curr:RouteSegment) {
+        this.id = curr.getParam('id');
+        console.log(this.id);
+
     }
 
 }
