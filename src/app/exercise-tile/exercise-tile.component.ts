baad6da1d1405e829d52d2a9d5e27f7dea2347f9
Nathan Blanka <healbadbad@yahoo.com>
2016-05-15 19:57:38 -0400

    final commit

--- src/app/exercise-tile/exercise-tile.component.ts
+++ src/app/exercise-tile/exercise-tile.component.ts
@@ -11,12 +11,18 @@ import {Exercise} from "../models/exercise";
 
 export class ExerciseTileComponent implements OnInit {
   public exercise: Exercise;
+  private imgurl: string;
 
   constructor() {
   }
 
   ngOnInit() {
+  // console.log(this.exercise);
+  //   this.imgurl = "http://img.youtube.com/vi/" +
+  //    this.exercise.videoUrl.split("embed/")[1].split("?")[0] +
+  //    "0.jpg"
 
+  //    //https://www.youtube.com/embed/K2e4lNrUXYo?loop=1?modestbranding=1
   }
 
 
