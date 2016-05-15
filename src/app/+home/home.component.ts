85f1d81c8ca917071755c111f195e1abe67e13f1
chinchalupa <wrightjt@rose-hulman.edu>
2016-05-15 08:29:46 -0400

    Moved buttons to modal and added sign up buttons
    Buttons now organized in a manner that makes sense.
    Waiting on sign up routing and proper database initialization.

--- src/app/+home/home.component.ts
+++ src/app/+home/home.component.ts
@@ -1,12 +1,17 @@
 import{ Component, OnInit } from '@angular/core';
 import {ProfileService} from "../services/profile/profile.service";
 import { Router } from '@angular/router';
+import {SigninModalComponent} from "../signin-modal/signin-modal.component";
+import {MaterializeDirective} from "angular2-materialize";
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
+
+  directives: [SigninModalComponent, MaterializeDirective],
   providers: [ProfileService]
 
 })
@@ -15,5 +20,17 @@ export class HomeComponent implements OnInit {
   constructor(private profileService: ProfileService, private router: Router) {}
 
   ngOnInit() {
+  }
+
+  onFacebookSignup() {
+
+  }
+
+  onGithubSignup() {
+
+  }
+
+  onGoogleSignup() {
+
   }
 }
