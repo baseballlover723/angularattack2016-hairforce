451f8b47149323b6fffb2c16c7c85d6ed827bc92
rosspa <rosspa@rose-hulman.edu>
2016-05-15 18:59:10 -0400

    implemented get workouts with objects

--- src/app/services/workout/workout.service.ts
+++ src/app/services/workout/workout.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import {AngularFire} from "angularfire2";
 import {Workout} from "../../models/workout";
+import {AssignmentService} from "../assignment/assignment.service";
 
 @Injectable()
 export class WorkoutService {
 
-  constructor(private af: AngularFire) {}
+  constructor(private af: AngularFire, private assignmentService: AssignmentService) {}
 
   getWorkout(id: string, callback = (workout) => {}) {
     let sub = this.af.object("/workouts/" + id).subscribe((workout) => {
@@ -17,6 +18,15 @@ export class WorkoutService {
       workout.$key = id;
       callback(workout);
       return;
+    });
+  }
+
+  getWorkoutWithObject(id: string, callback = (workout) => {}) {
+    this.getWorkout(id, (workout) => {
+      this.assignmentService.getAssignmentsWithObjects(workout.assignments, (assignments) => {
+        workout.assignments = assignments;
+        callback(workout);
+      });
     });
   }
 
@@ -37,11 +47,44 @@ export class WorkoutService {
     }
   }
 
+  getWorkoutsWithObjects(ids: string[], callback = (workouts) => {}) {
+    var workouts = [];
+    for (var index in ids) {
+      var id = ids[index];
+      this.getWorkoutWithObject(id, (workout) => {
+        if (workout) {
+          workouts.push(workout);
+        } else {
+          ids.splice(parseInt(index), 1);
+        }
+        if (ids.length == workouts.length) {
+          callback(workouts);
+        }
+      });
+    }
+  }
+
   getAllWorkouts(callback = (workouts) => {}) {
     let sub = this.af.list("/workouts/").subscribe((workouts) => {
       sub.unsubscribe();
       callback(workouts);
       return;
+    });
+  }
+
+  getAllWorkoutsWithObjects(callback = (workouts) => {}) {
+    this.getAllWorkouts((workouts) => {
+      let count = 0;
+      for (let index in workouts) {
+        let workout = workouts[index];
+        this.assignmentService.getAssignmentsWithObjects(workout.assignments, (assignments) => {
+          workout.assignments = assignments;
+          count++;
+          if (count == workouts.length) {
+            callback(workouts);
+          }
+        });
+      }
     });
   }
 
